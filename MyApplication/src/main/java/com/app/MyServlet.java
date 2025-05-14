84fec0cc719a455bd8241368f22214910079713b
Digvijay Devare <digvijaydevare@gmail.com>
2025-05-14 17:44:32 +0530

    init 1

---
+++ MyApplication/src/main/java/com/app/MyServlet.java
@@ -0,0 +1,56 @@
+package com.app;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");//MIME Type
+		PrintWriter pw = response.getWriter();
+		
+		String uname = request.getParameter("txtUserName");
+		String pwd = request.getParameter("txtPAssword");
+		
+		if((uname.equals("admin"))&& (pwd.equals("admin123")))
+		{
+			pw.write("<h1>Login Sucessfull</h1>"+uname);
+		}
+		else
+		{
+			pw.write("<h1> Invalid Username and password </h1>");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
